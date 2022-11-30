7a93263f56931eb562d2f1a5f3a461745ef79cef
Reece Bradley <naruto73@hotmail.co.uk>
2022-11-30 15:38:45 +0000

    create funtion to call function and operator on 2 numbers

--- script.js
+++ script.js
@@ -14,4 +14,23 @@ function divide(a, b) {
   return a / b;
 };
 
-
+function operate(num1, operator, num2) {
+  switch(operator) {
+    case '+':
+      return add(num1, num2)
+      break;
+    case '-':
+      return subtract(num1, num2)
+      break;
+    case '*':
+      return multiply(num1, num2)
+      break;
+    case '/':
+      if (num2 === 0) {
+        alert("Can't divide by 0!");
+      } else {
+        return divide(num1, num2)
+      };
+      break;
+  };
+};
