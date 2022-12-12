afdf1e9ede00b92de7cc2a97f6a6f21e7f37e8dc
Reece Bradley <naruto73@hotmail.co.uk>
2022-12-12 23:30:37 +0000

    Tidy up code so everything works

--- script.js
+++ script.js
@@ -1,50 +1,143 @@
 const display = document.querySelector('.display');
 const numberButtons = document.querySelectorAll('.num-btns');
+const operatorButtons = document.querySelectorAll('.op-btns');
+const equalButton = document.querySelector('#equal-btn');
+const functionButtons = document.querySelectorAll('.function-btn');
+
+let firstInput = 0;
+let secondInput = 0;
+let operatorInput = '';
+
+function toDefault() {
+  firstInput = 0;
+  secondInput = 0;
+  operatorInput = 0;
+};
 
 // Create function to display pressed numbers on the calculator screen
 numberButtons.forEach(numButton => {
   numButton.addEventListener('click', () => {
     let btnContent = numButton.innerText;
-    display.textContent = btnContent;
+    if(display.textContent.length >= 11) {
+      return;
+    };
+    if(btnContent === '.' && display.textContent.includes('.')) {
+      return;
+    } else if(display.textContent.includes('Infinity') || display.textContent.includes('NaN') || display.textContent.includes("error")) {
+      display.textContent = btnContent;
+      toDefault();
+    } else {
+      display.textContent += btnContent;
+    };
+  });
+});
+
+// Create function to store a number and operator in a variable
+operatorButtons.forEach(opButton => {
+  opButton.addEventListener('click', () => {
+    if(display.textContent === '' && operatorInput === '') {
+      return;
+    };
+    if(firstInput === 0 && display.textContent !== '') {
+      firstInput = display.textContent;
+      operatorInput = opButton.innerText;
+      display.textContent = '';
+    } else {
+      secondInput = display.textContent;
+      operate(firstInput, operatorInput, secondInput);
+      firstInput = display.textContent;
+      operatorInput = opButton.innerText;
+      display.textContent = '';
+    };
+  });
+});
+
+// Create function to display the answer to your sum
+equalButton.addEventListener('click', () => {
+  secondInput = display.textContent;
+  if(operatorInput === '') {
+    return;
+  } else {
+    operate(firstInput, operatorInput, secondInput);
+    toDefault();
+  };
+});
+
+// Create function to do a function buttons task when clicked
+functionButtons.forEach(funcButton => {
+  funcButton.addEventListener('click', () => {
+    let btnContent = funcButton.innerText;
+    if(btnContent === 'AC') {
+      display.textContent = '';
+      toDefault();
+    };
+    if(btnContent === 'C') {
+      if(display.textContent === '') {
+        return;
+      } else {
+        display.textContent = display.textContent.slice(0, -1);
+      };
+    };
+    if(btnContent === '+/-') {
+      if(display.textContent === '') {
+        return;
+      } else {
+        display.textContent = -(parseFloat(display.textContent));
+      };
+    };
+    if(btnContent === '%') {
+      if(display.textContent === '') {
+        return;
+      } else {
+        display.textContent = (parseFloat(display.textContent) / 100);
+      };
+    };
   });
 });
 
 // Create basic math functions
 function add(a, b) {
-  return a + b;
+  return parseFloat(a) + parseFloat(b);
 };
 
 function subtract(a, b) {
-  return a - b;
+  return parseFloat(a) - parseFloat(b);
 };
 
 function multiply(a, b) {
-  return a * b;
+  return parseFloat(a) * parseFloat(b);
 };
 
 function divide(a, b) {
-  return a / b;
+  if(parseFloat(b) === 0) {
+    return "error"
+  } else {
+    return parseFloat(a) / parseFloat(b);
+  };
 };
 
 //Create function that takes 2 numbers and an operator and then calls a math function
 function operate(num1, operator, num2) {
+  let result = '';
   switch(operator) {
     case '+':
-      return add(num1, num2)
+      result = add(num1, num2)
+      display.textContent = result.toString().slice(0, 11)
       break;
     case '-':
-      return subtract(num1, num2)
+      result = subtract(num1, num2)
+      display.textContent = result.toString().slice(0, 11)
       break;
-    case '*':
-      return multiply(num1, num2)
+    case 'x':
+      result = multiply(num1, num2)
+      display.textContent = result.toString().slice(0, 11)
       break;
     case '/':
-      if (num2 === 0) {
-        alert("Can't divide by 0!");
-      } else {
-        return divide(num1, num2)
-      };
+      result = divide(num1, num2)
+      display.textContent = result.toString().slice(0, 11)
       break;
+    default:
+      return
   };
 };
 
